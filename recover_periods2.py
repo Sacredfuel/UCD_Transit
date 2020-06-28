7004e399e29d22274a8d6009b261b6bbbcd78cea
Sacredfuel <45538636+Sacredfuel@users.noreply.github.com>
2020-06-28 10:01:42 -0400

    Add files via upload

--- recover_periods2.py
+++ recover_periods2.py
@@ -5,20 +5,22 @@ from astropy.timeseries import BoxLeastSquares
 import pandas as pd
 import numpy as np
 import lightkurve as lk
-import math
 plt.rc('text', usetex=False)
 sns.set_context('talk')
-
-df = pd.DataFrame(columns = ['EPIC','PERIOD','DEPTH','TRANSITTIME','Rp/Rs'])
 # how to 'SpT','Rs','Rp'
 #['a', 'b', 'c', 'd', 'e']
 #'EPIC','PERIOD','DEPTH','TRANSITTIME','Rp/Rs'
 
+df = pd.DataFrame(columns = ['EPIC','PERIOD','DEPTH','TRANSITTIME','Rp/Rs'])
+
 def currentFrame():
-    return df
+    print(df)
+
+def addToFrame(lis):
+    df.append(lis)
 
 class model:
-    def __init__(self, EPIC, K2name, minperiod = 1, maxperiod = 30, window_length=301, sigma=10, addToFrame = True):
+    def __init__(self, EPIC, K2name, maxperiod = 30, window_length=301, sigma_step=1, sigma_max = 10, debug_mode = False):
         self.K2name = K2name
         self.EPIC = EPIC
         for i in np.arange(1,18,1):
@@ -27,13 +29,17 @@ class model:
             except Exception as e:
                 i+=i
 
+        try:
+            self.star
+        except Exception as e:
+            raise NameError("EPIC ID has no cooresponding Star")    
+
         self.rawlc = lk.LightCurve(time = self.star.time, flux = self.star.flux)
 
-        self.flat = self.rawlc.flatten(window_length=window_length).remove_outliers(sigma=sigma)
+        sigma = self.__findBestSigma(maxperiod, window_length, sigma_step, sigma_max, debug_mode)
+        self.__generateFlat(window_length, sigma)
+        self.__generatePeriodogram(maxperiod)
 
-        model = BoxLeastSquares(self.flat.time, self.flat.flux, dy=0.01)
-        testperiods = np.arange(minperiod, maxperiod, 0.001)
-        self.periodogram = model.power(testperiods, 0.16)
         maxID = np.argmax(self.periodogram.power)
         self.best_fit_period = self.periodogram.period[maxID]
         self.transit_time = self.periodogram.transit_time[maxID]
@@ -41,9 +47,42 @@ class model:
 
         self.foldedlc = self.flat.fold(period = self.best_fit_period, t0 = self.transit_time)
 
-        if(addToFrame==True):
-            df.append([self.EPIC, self.best_fit_period, self.transit_depth, self.transit_time, math.sqrt(self.transit_depth)])
+    def __findBestSigma(self, maxperiod, window_length, sigma_step, sigma_max, debug_mode = False):
+        i=3 #starting point
+        self.sigArr = []
+        self.lenArr = []
+        self.maxdataArr = []
 
+        while(sigma_max>=i):
+            flat = self.rawlc.flatten(window_length=window_length).remove_outliers(sigma=i)
+            model = BoxLeastSquares(flat.time, flat.flux, dy=0.01)
+            testperiods = np.arange(1, maxperiod, 0.001)
+            periodogram = model.power(testperiods, 0.16)
+            maxID = np.argmax(periodogram.power)
+            stat = model.compute_stats(periodogram.period[maxID], periodogram.duration[maxID] , periodogram.transit_time[maxID])
+            self.sigArr.append(sum(stat['per_transit_log_likelihood']))
+            self.lenArr.append(len(stat['per_transit_log_likelihood']))
+            if debug_mode:
+                print([i, sum(stat['per_transit_log_likelihood']),len(stat['per_transit_log_likelihood']), periodogram.period[maxID]]) #Debug
+            i+=sigma_step
+        
+        maxLLikeIndex = np.argwhere(self.lenArr == np.amax(self.lenArr))
+        for i in maxLLikeIndex:
+            self.maxdataArr.append(self.sigArr[i.item(0)])
+        bestFit = np.argmax(self.maxdataArr) - 1
+        if debug_mode:
+            print("Index of LogLikelihoods")
+            print(maxLLikeIndex)
+            print("Best Sigma")
+            print(bestFit+i)
+        
+        return bestFit + i
+    def __generateFlat(self, window_length, bestfitsigma):
+        self.flat = self.rawlc.flatten(window_length=window_length).remove_outliers(sigma=bestfitsigma)
+    def __generatePeriodogram(self, maxperiod):
+        model = BoxLeastSquares(self.flat.time, self.flat.flux, dy=0.01)
+        testperiods = np.arange(1, maxperiod, 0.001)
+        self.periodogram = model.power(testperiods, 0.16)
 
     def dvs(self):
         return self.star.dvs()
@@ -87,4 +126,8 @@ class model:
         df['Rp/Rs'] = np.sqrt(transit_depth)
         df['Rp'] = df['Rs']*df['Rp/Rs'] / REarth
         return df
+    def showStats(self):
+        print(self.sigArr)
+        print(str(self.bestfitsigma))
+    
 
